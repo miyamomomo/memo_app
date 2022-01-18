eb3fa312a7a577657c0bbb051042408408162216
miyamomomomo <62239929+miyamomomo@users.noreply.github.com>
2022-01-18 18:17:02 +0900

    Update memo_app.py

--- memo_app.py
+++ memo_app.py
@@ -25,3 +25,68 @@ def list():
     posts = Post.query.all()
 
     return render_template('list.html', message = message, posts = posts)
+
+
+@app.route('/show/<int:id>')
+def show_post(id):
+
+    message = 'Your memo ' + str(id)
+    post = Post.query.get(id)
+
+    return render_template('show.html', message = message, post = post)
+
+
+@app.route('/new')
+def new_post():
+
+    message = 'New memo'
+    return render_template('new.html', message = message)
+
+
+@app.route('/create', methods=['POST'])
+def create_post():
+
+    message = 'create your memo'
+
+    new_post = Post()
+    new_post.title = request.form['title']
+    new_post.content = request.form['content']
+    db.session.add(new_post)
+    db.session.commit()
+
+    post = Post.query.get(new_post.id)
+
+    return render_template('show.html', message = message, post = post)
+
+
+@app.route('/destroy/<int:id>')
+def destroy_post(id):
+
+    message = 'delete memo ' + str(id)
+
+    destroy_post = Post.query.get(id)
+    db.session.delete(destroy_post)
+    db.session.commit()
+
+    posts = Post.query.all()
+
+    return render_template('list.html', message = message, posts = posts)
+
+@app.route('/edit/<int:id>')
+def edit_post(id):
+
+    message = 'Edit memo'+str(id)
+    post=Post.query.get(id)
+    return render_template('edit.html', message = message,post=post)
+    
+@app.route('/update/<int:id>', methods=['POST'])
+def update_post(id):
+
+    message = 'update your memo'
+
+    post = Post.query.get(id)
+    post.title = request.form['title']
+    post.content = request.form['content']
+    db.session.commit()
+
+    return render_template('show.html', message = message, post = post)
